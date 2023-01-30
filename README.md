1a70f018c635bc188bfeee12b7eb5cdf57fb394f
Kerry <bryanmayodi@gmail.com>
2023-01-30 11:01:16 +0300

    project complete

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # simple_calculator
 this a simple project to help me learn JavaScript
 it also incoorporates html and css
+live site at https://milk-in-a-bag.github.io/simple_calculator/
--- script.js
+++ script.js
@@ -20,6 +20,7 @@ class Calculator{
     }
 
     delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
 
     }
 
@@ -39,12 +40,59 @@ class Calculator{
     }
 
     compute(){
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if(isNaN(prev)||isNaN(current)) return;
 
+        switch(this.operation){
+            case "+":
+                computation = prev + current;
+                break
+            case "-":
+                computation = prev - current;
+                break
+            case "*":
+                computation = prev *  current;
+                break
+            case "÷":
+                computation = prev / current;
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = "";
     }
 
+    getDisplayNumber(number){
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split(".")[0]);
+        const decimalDigits = stringNumber.split(".")[1];
+        let integerDisplay;
+        if(isNaN(integerDigits)){
+            integerDisplay = "";
+        }
+        else{
+            integerDisplay = integerDigits.toLocaleString("en", {
+                maximumFractionDigits: 0 })
+        }
+        if(decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        }
+        else{
+            return integerDisplay
+        }
+    }
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand;
-        this.previousOperandTextElement.innerText = this.previousOperand;
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+        }
+        else{
+            this.previousOperandTextElement.innerText = "";
+        }
     }
 }
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
@@ -63,7 +111,17 @@ operationButtons.forEach(button => {
     })
 })
 
-equalsButton.addEventListener("click", () => {
+equalsButton.addEventListener("click", button => {
     calculator.compute();
+    calculator.updateDisplay();
+})
+
+allClearButton.addEventListener("click", button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener("click", button => {
+    calculator.delete();
     calculator.updateDisplay();
 })
